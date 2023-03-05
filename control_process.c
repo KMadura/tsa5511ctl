d3e504a0b34b23ae666007879930aebc61061b3a
Krzysztof Madura <krzysztof.madura@protonmail.com>
2023-03-05 21:57:41 +0100

    Further explanation what these threads are meant to do

--- control_process.c
+++ control_process.c
@@ -1,5 +1,26 @@
+#include <pthread.h>
+
+pthread_mutex_t mutex_mon = PTHREAD_MUTEX_INITIALIZER;
+
 void* start_monitoring_process() {
-    sleep(10);
+    while(true) {
+        pthread_mutex_lock(&mutex_mon);
+
+        /* TODO: check if device is still running, if not try to reconnect */
+
+        /* TODO: check if frequency is set to required one, if not then change it */
+
+        /* TODO: check if lock is set to current required value, if not try to change it */
+
+        /* TODO: add more checks */
+
+        pthread_mutex_unlock(&mutex_mon);
+
+        sleep(20);
+
+        // Remove this break, it exists only for debugging purposes
+        break;
+    }
 
     return NULL;
 }
--- network_process.c
+++ network_process.c
@@ -1,5 +1,18 @@
+#include <pthread.h>
+
+pthread_mutex_t mutex_net = PTHREAD_MUTEX_INITIALIZER;
+
 void* start_networking_process() {
-    sleep(10);
+    /*
+     * TODO: implement a network loop
+     * this function should seek UDP packets and respond to them
+     * some of those queries will only receive values stored in this program / daemon
+     * some other of those queries will directly instruct TSA5511 chip
+     * any communication with TSA5511 should use mutex to prevent race condition with
+     * control thread
+     */
+
+    sleep(20);
 
     return NULL;
 }
